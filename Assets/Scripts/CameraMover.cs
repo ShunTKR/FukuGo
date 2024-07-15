ab23450d74df72be9a5421f59582900374c41425
Shun <tkrshun0408@gmail.com>
2024-07-15 19:22:53 +0900

    カメラ移動機能をDOTweenで実装

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using DG.Tweening;
 
 public class CameraMover : MonoBehaviour
 {
+    [SerializeField] private Transform[] _targets;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _duration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.DOPath(
+            path: _targets.Select(x => x.position + _offset).ToArray(),
+            duration: _duration,
+            pathType: PathType.Linear
+        ).SetEase(Ease.Linear).SetLookAt(0.001f, Vector3.forward);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
